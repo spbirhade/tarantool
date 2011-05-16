da2eee286453d227de5c6d95360bb00c76ba4081
Dmitry Simonenko <pmwkaa@gmail.com>
2011-05-16 11:45:58 +0400

    * License typo fix.

--- connector/c/tnt_aes.c
+++ connector/c/tnt_aes.c
@@ -3,7 +3,7 @@
  * Copyright (C) 2011 Mail.RU
  *
  * Redistribution and use in source and binary forms, with or without
- * modification, are permitnted provided that the following conditions
+ * modification, are permitted provided that the following conditions
  * are met:
  * 1. Redistributions of source code must retain the above copyright
  *    notice, this list of conditions and the following disclaimer.
